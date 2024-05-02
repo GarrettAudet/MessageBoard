279da07808eece625d8fd313fe96968ad98ab988
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-05-02 16:54:52 -0400

    Added Sample Messages and HTML in the Index Template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: "Mini Messageboard", 
+    messages: messages  // This passes the messages array to the view
+  });
 });
 
 module.exports = router;
