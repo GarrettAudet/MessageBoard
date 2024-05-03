d812ea90e9b083ec808d26a55af34b91626b1e64
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-05-03 02:19:31 -0400

    Created the MessageBoard

--- routes/new.js
+++ routes/new.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const { messages } = require(./index.js)
+const { messages } = require('./index.js');
   
 /* GET home page. */
-router.get('/new', function(req, res, next) {
-    res.render('form', { 
+router.get('/', function(req, res, next) {
+    res.render('new', { 
         title: "Movie Messageboard", 
     });
 });
 
 /* POST new message form submission */
-router.post('/new', function(req, res, next) {
+router.post('/', function(req, res, next) {
     var author = req.body.messageUser;
     var message = req.body.messageText;
     messages.push({text: message, user: author, added: new Date()});
